6254171d9cfdd8c4da17f52285fcc4c0e4cb3920
Chris Troutner <chris.troutner@gmail.com>
2018-01-10 15:51:48 -0800

    Chaning port to 3000

--- doc-server.js
+++ doc-server.js
@@ -5,7 +5,7 @@ const express = require("express");
 
 // Global Variables
 const app = express();
-const port = 3000;
+const port = 8000;
 
 // Create an Express server.
 const ExpressServer = require("./lib/express-server.js");
