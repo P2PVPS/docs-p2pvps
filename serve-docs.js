97299a9c43cef442b8d5cf60a5d8af9ff4f0a389
Chris Troutner <chris.troutner@gmail.com>
2018-01-17 14:39:51 -0800

    Adding client-server registration workflow to documentation

--- serve-docs.js
+++ serve-docs.js
@@ -5,7 +5,7 @@ const express = require("express");
 
 // Global Variables
 const app = express();
-const port = 8000;
+const port = 8124;
 
 // Create an Express server.
 const ExpressServer = require("./lib/express-server.js");
